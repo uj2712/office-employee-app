b6ef85c74afab6a8601537136260e7fe31713a66
Ujjawal Pratap Singh <92955912+uj2712@users.noreply.github.com>
2022-10-12 21:40:41 +0530

    Update models.py

--- employee_app/models.py
+++ employee_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Department(models.Model):
     name=models.CharField(max_length=100,null=False)
-    location=models.CharField(max_length=100)\
+    location=models.CharField(max_length=100)
     
     def __str__(self):
         return self.name
